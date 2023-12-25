d1e643bc06521e78b21f8ee97d9607371e53bfcb
ItsGotToBeThere <58275224+ItsGotToBeThere@users.noreply.github.com>
2023-12-25 12:58:06 -0500

    Update thirdtry.js

--- Tic-Tac-Toes/Ultimate/thirdtry.js
+++ Tic-Tac-Toes/Ultimate/thirdtry.js
@@ -1,4 +1,5 @@
 const smallgrids = document.querySelectorAll('.smallgrid');
+const body = document.querySelector('body')
 let activesquares = document.querySelectorAll('.smallgrid.active > div')
 const directiontext = document.querySelector('#direction-text')
 const winningtext = document.getElementById('winning-text')
@@ -58,6 +59,7 @@ let largegrid = [
 
 document.addEventListener('click',function(event){
     if (event.target.parentElement.classList.contains('active')){
+        directiontext.textContent = ""
         const square = event.target;
         smallgrids.forEach(function(smallgrid){
             smallgrid.classList.remove('hover-state')
@@ -69,32 +71,34 @@ document.addEventListener('click',function(event){
             const board = getBoard(square)
             const x = getX(square)
             const y = getY(square)
-            directiontext.textContent = "this part is working"
             if (count%2) {
                 background= "#d62828"
                 playerid = 2
             }
             else {
-                background = "#fcbf49"
+                background = "#fcbf49" 
                 playerid =  1
             }
             square.style.background=background
             matrix[board][y][x]=playerid
             count++
-            directiontext.textContent=count
             if (win(x,y,board,playerid)){
-                winningtext.textContent = `The ${y}${x} square has been won!`
-                winningtext.style.color=background
                 square.parentElement.classList.add('won')
                 const largegridx = square.parentElement.id.charAt(1);
                 const largegridy = square.parentElement.id.charAt(0);
                 largegrid[largegridy][largegridx]=playerid
-                directiontext.textContent = largegrid
                 document.querySelectorAll('.smallgrid.active > div').forEach(function(square){
                     square.parentElement.style.background = background;
                     square.style.background=background;
                     square.classList.add('clicked')
                 })
+                if (largewin(largegridx,largegridy,largegrid,playerid)){
+                    body.style.background = background
+                    smallgrids.forEach(function(smallgrid){
+                        smallgrid.classList.add('won')
+                        winningtext.textContent = `Player ${playerid} has Won!`
+                    })
+                }
             }
             document.querySelectorAll(".smallgrid").forEach(function(smallgrid){
                 smallgrid.classList.remove("active")
@@ -127,6 +131,19 @@ function getY(square){
     return y
 }
 
+function largewin(x,y,board,playerid){
+    let row = true;
+    let column = true;
+    let diagup = true;
+    let diagdown = true;
+    for (let i = 0;i<3;i++){
+        row&=board[y][i]===playerid;
+        column&=board[i][x]===playerid;
+        diagup&=board[2-i][i]===playerid;
+        diagdown&=board[i][i]===playerid
+    }
+    return row || column || diagup || diagdown
+}
 
 function win(x,y,board,playerid){
     let row = true;
@@ -146,8 +163,8 @@ function win(x,y,board,playerid){
 
 function PickAnyBoard(){
     smallgrids.forEach(function(smallgrid){
-        smallgrid.classList.add('hover-state')
         if (!smallgrid.classList.contains('won')){
+        smallgrid.classList.add('hover-state')
         hover(smallgrid)
         unhover(smallgrid)
         }
@@ -155,7 +172,7 @@ function PickAnyBoard(){
     
     function hover(smallgrid){
         smallgrid.addEventListener('mouseenter',function(){
-            if (smallgrid.classList.contains('hover-state')&&!smallgrid.classList.contain('won')){
+            if (smallgrid.classList.contains('hover-state')){
             smallgrid.classList.add('active')
             }
         })
@@ -163,7 +180,7 @@ function PickAnyBoard(){
     
     function unhover(smallgrid){
         smallgrid.addEventListener('mouseleave',function(){
-            if (smallgrid.classList.contains('hover-state')&&!smallgrid.classList.contain('won')){
+            if (smallgrid.classList.contains('hover-state')){
                 smallgrid.classList.remove('active')
                 }
         })
